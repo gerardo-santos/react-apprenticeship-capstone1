c2cd7215c084af542ce939de76990f9c12678824
Gerardo Santos Gonzalez <97237961+gerardo-santos@users.noreply.github.com>
2022-01-15 12:45:36 -0600

    feat: added favorite details page

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,6 +3,7 @@ import Home from '../pages/Home';
 import VideoDetails from '../pages/VideoDetails';
 import LogIn from '../pages/LogIn';
 import Favorites from '../pages/private/Favorites';
+import FavoriteDetails from '../pages/private/FavoriteDetails';
 
 const AppRouter = () => {
   return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
       <Route path="video-details/:id" element={<VideoDetails />} />
       <Route path="log-in" element={<LogIn />} />
       <Route path="favorites" element={<Favorites />} />
+      <Route path="favorite-details/:id" element={<FavoriteDetails />} />
     </Routes>
   );
 };
