f61ca05c478bc7b398ca473bc6ba780d1ffc123e
Gerardo Santos Gonzalez <97237961+gerardo-santos@users.noreply.github.com>
2022-01-19 15:45:19 -0600

    feat: added clear local storage argument

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,8 +1,12 @@
 import { useEffect } from 'react';
 
-const useLocalStorage = (key, state) => {
+const useLocalStorage = (key, state, clearLocalStorage) => {
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
+    if (clearLocalStorage) {
+      localStorage.setItem(key, JSON.stringify(null));
+    } else {
+      localStorage.setItem(key, JSON.stringify(state));
+    }
   }, [state]);
 };
 
